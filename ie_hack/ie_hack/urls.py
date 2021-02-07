d48d423c080e8e91715f244dca158e5c362ef3c9
raj1701 <rajatpartani@gmail.com>
2021-02-07 10:16:35 +0530

    merged views

--- ie_hack/ie_hack/urls.py
+++ ie_hack/ie_hack/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [ 
 	path('admin/', admin.site.urls), 
-	path('', views.home), 
-    path("login_firebase",views.login_firebase, name="login" ),
-    path("firebase_login_save",views.firebase_login_save),
-    path('events/',include('hack.urls'))
+    path('',include('hack.urls'))
 ] 
