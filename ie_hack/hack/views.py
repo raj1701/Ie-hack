b64390053bbf6ad94f8f93688ab9ecb6c2d4887d
raj1701 <rajatpartani@gmail.com>
2021-02-07 10:26:12 +0530

    merged views

--- ie_hack/hack/views.py
+++ ie_hack/hack/views.py
@@ -116,26 +116,31 @@ def home(request):
     events = Event.objects.filter(active=True)
     return render(request, "dashboard.html",{"user": request.user , "events": events})
 
+
+
 def create_form(request):
      return render(request,"create_form.html")
+
+
 
 def create_event(request):
     if not request.user.is_authenticated:
         return redirect('/login_firebase')
-    if not name or not desc or not venue or not sig:
-        return redirect('/events')
+
     
     name = request.POST.get("name")    
     desc = request.POST.get("desc")    
     venue = request.POST.get("venue")    
     sig = request.POST.get("sig")
     date= request.POST.get('date')    
+    if not name or not desc or not venue or not sig:
+        return redirect('/')
     event  = Event(name=name, desc= desc, venue= venue, sig=sig, date= date, active=True)
     event.save()
     current_user = User.objects.get(id = request.user.id)
     event.mentors.add(current_user)
     event.save()
-    return redirect('/events')
+    return redirect('/')
 
 def add_resource(request,event_id):
     event = Event.objects.get(pk =event_id)
@@ -146,7 +151,7 @@ def add_resource(request,event_id):
         return redirect('/login_firebase')
 
     if not request.user in event.mentors.all():
-        return redirect('/events')
+        return redirect('/')
     resource = Resource(name= request.POST.get("name"), link=request.POST.get("link"), event=event, author=request.user)  
     resource.save()
-    return redirect('/events')
+    return redirect('/')
